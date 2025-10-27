7ebb5e58bb902fc42088ce0001fd5fbe4d222f8d
Jose <hurtadorivasj123@gmail.com>
2025-10-27 18:51:24 -0500

    Pagina de ERROR 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,13 @@ import { LoginComponent } from './pages/login/login';
 import { DashboardProfesorComponent } from './pages/dashboard-profesor/dashboard-profesor.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { CreateEvaluationComponent } from './pages/create-evaluation/create-evaluation.component';
+import { Error404Component } from './pages/error-404/error-404.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Redirige a login por defecto
   { path: 'dashboard_profesor', component: DashboardProfesorComponent },
   { path: "details", component:DetailsComponent},
-  {path: "createEvaluation" , component:CreateEvaluationComponent}
+  {path: "createEvaluation" , component:CreateEvaluationComponent},
+  { path: "**", component:Error404Component}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
