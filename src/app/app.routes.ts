b47021c514c1268ddc1337f61fbcc0f7a5d65b1a
Jose <hurtadorivasj123@gmail.com>
2025-10-27 17:41:41 -0500

    Transferencia a pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './component/login/login';
-import { DashboardProfesorComponent } from './component/dashboard-profesor/dashboard-profesor.component';
-import { DetailsComponent } from './component/details/details.component';
-import { CreateEvaluationComponent } from './component/create-evaluation/create-evaluation.component';
+import { LoginComponent } from './pages/login/login';
+import { DashboardProfesorComponent } from './pages/dashboard-profesor/dashboard-profesor.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { CreateEvaluationComponent } from './pages/create-evaluation/create-evaluation.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Redirige a login por defecto
