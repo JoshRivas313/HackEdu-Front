d8970884a1651d0d849565a100c42cc2977be962
Jose <hurtadorivasj123@gmail.com>
2025-10-27 15:49:21 -0500

    Vista Create Evaluation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login';
 import { DashboardProfesorComponent } from './component/dashboard-profesor/dashboard-profesor.component';
 import { DetailsComponent } from './component/details/details.component';
+import { CreateEvaluationComponent } from './component/create-evaluation/create-evaluation.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Redirige a login por defecto
   { path: 'dashboard_profesor', component: DashboardProfesorComponent },
-  { path: "details", component:DetailsComponent}
+  { path: "details", component:DetailsComponent},
+  {path: "createEvaluation" , component:CreateEvaluationComponent}
 ];
 
 @NgModule({
---
+++ src/app/component/create-evaluation/create-evaluation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-evaluation',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-evaluation.component.html',
+  styleUrl: './create-evaluation.component.scss'
+})
+export class CreateEvaluationComponent {
+
+}
