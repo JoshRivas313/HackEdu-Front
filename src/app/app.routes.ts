0e67e0101309f4a6bd13feb46ca3103a9b1b16b5
Jose <hurtadorivasj123@gmail.com>
2025-10-27 13:01:40 -0500

    Vista details realizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login';
-import { DashboardProfesorComponent } from './component/dashboard-profesor/dashboard-profesor.component'; 
+import { DashboardProfesorComponent } from './component/dashboard-profesor/dashboard-profesor.component';
+import { DetailsComponent } from './component/details/details.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Redirige a login por defecto
-  { path: 'dashboard_profesor', component: DashboardProfesorComponent }
+  { path: 'dashboard_profesor', component: DashboardProfesorComponent },
+  { path: "details", component:DetailsComponent}
 ];
 
 @NgModule({
