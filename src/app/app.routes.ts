7d02bf585804d6f25a369fe299f2fb807db00919
Jose <hurtadorivasj123@gmail.com>
2025-10-28 06:57:36 -0500

    Login Utilizable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
-import { DashboardProfesorComponent } from './pages/dashboard-profesor/dashboard-profesor.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { CreateEvaluationComponent } from './pages/create-evaluation/create-evaluation.component';
 import { Error404Component } from './pages/error-404/error-404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent , pathMatch:'full'  },
-  { path: 'dashboard_profesor', component: DashboardProfesorComponent , title:"Panel de Profesor" },
+  {path:"dashboard", component:DashboardComponent, title:"Panel de Control"},
   { path: "details", component:DetailsComponent, title:"Detalles"},
   {path: "createEvaluation" , component:CreateEvaluationComponent, title:"Formulario de Evaluaciones"},
   { path: "***",component:Error404Component}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,28 +2,44 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrls: ['./login.css']
+  styleUrls: ['./login.css'],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
-  remember: boolean = false;
-  errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
-  onSubmit() {
-    if (this.email === '1234' && this.password === '5678') {
-      this.errorMessage = '';
-      this.router.navigate(['/dashboard_profesor']);
+  Login() {
+    if (!this.email || !this.password) {
+      this.snackBar.open('Por favor complete todos los campos.', 'Cerrar', {
+        duration: 3000,
+        panelClass: ['bg-orange-500', 'text-white'],
+      });
+      return;
+    }
+
+    if (this.email === 'hurtadorivasj123@gmail.com' && this.password === '123') {
+
+      setTimeout(() => {
+        this.router.navigate(['/dashboard']);
+      }, 1000);
+
     } else {
-      this.errorMessage = 'Usuario o contraseña incorrectos';
+      this.snackBar.open('Correo o contraseña incorrectos ❌', 'Cerrar', {
+        duration: 3000,
+        panelClass: ['bg-red-500', 'text-white'],
+      });
     }
   }
 }
